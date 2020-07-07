5ea23d576b69e916c3822a96a0659898f8c09293
Christopher Lang <chrsalxlng@gmail.com>
2020-07-07 19:18:51 +0200

    js: added scroll animations to all buttons

--- js/index.js
+++ js/index.js
@@ -11,3 +11,37 @@ function toggleDropdownNav() {
         isDropdownNavOpen = true;
     } 
 }
+
+// ADDING EVENTLISTENERS TO BUTTONS
+addEventListernersToButtons("#home-btn", "#hero-section", true);
+addEventListernersToButtons("#features-btn", "#features-section", true);
+addEventListernersToButtons("#pricing-btn", "#pricing-section", true);
+addEventListernersToButtons("#signup-btn", "#signup-section", true);
+addEventListernersToButtons("#nav-signup-btn", "#signup-section", false);
+addEventListernersToButtons("#learnmore-btn", "#features-section", false);
+
+function addEventListernersToButtons(button, target, isNavLink) {
+    document.querySelector(button).addEventListener("click", function() {
+        scrollTo(target);
+
+        if (isNavLink) {
+            toggleDropdownNav();
+        }
+    });
+}
+
+function scrollTo(target) {
+    var elementSelector = document.querySelector(target);
+
+    const elementOffset = elementSelector.getBoundingClientRect().top;
+    const scrollPosition = window.scrollY;
+    const documentTop = document.documentElement.clientTop;
+    const scrollOffset = elementOffset + scrollPosition - documentTop;
+
+    anime({
+      targets: [document.documentElement, document.body],
+      scrollTop: scrollOffset - 45,
+      duration: 800,
+      easing: 'easeInOutQuad'
+    });
+}
